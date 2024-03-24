4182384288d827abd708a18c0112176564ddc28e
Talgat <akamypka@gmail.com>
2024-03-24 23:42:18 +0500

    feat: implement 12 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -259,8 +259,27 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  switch (date.getMonth()) {
+    case 0:
+    case 1:
+    case 2:
+      return 1;
+    case 3:
+    case 4:
+    case 5:
+      return 2;
+    case 6:
+    case 7:
+    case 8:
+      return 3;
+    case 9:
+    case 10:
+    case 11:
+      return 4;
+    default:
+      throw new Error('Invalid date');
+  }
 }
 
 /**
