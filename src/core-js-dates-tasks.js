ba2732aa4a81bbdf31f67e64016dee2065c66387
Talgat <akamypka@gmail.com>
2024-03-26 00:27:47 +0500

    feat: implement isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -316,8 +316,10 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const yearToCheck = new Date(date.getFullYear(), 1, 29);
+
+  return yearToCheck.getDate() === 29;
 }
 
 module.exports = {
